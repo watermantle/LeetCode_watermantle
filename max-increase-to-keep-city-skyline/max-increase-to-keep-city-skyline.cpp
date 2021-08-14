45edeb60430a2ada9f8081ae015d6ad8940d26b8
Yu Bai <43456665+watermantle@users.noreply.github.com>
2021-08-14 01:14:17 -0400

    Time: 24 ms (7.48%), Space: 10.2 MB (24.84%) - LeetHub

--- max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
+++ max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-
-        vector<int> row_maxes;
+        int n = grid.size();
+        int res = 0;
         vector<int> col_maxes;
-
-        for (auto row : grid) {
-            int row_max = *max_element(row.begin(), row.end());
-            row_maxes.push_back(row_max);
-        }
 
         for (int i = 0; i != n; i++) {
             int col_max = 0;
-            for (int j = 0; j != m; j++) {
+            for (int j = 0; j != n; j++) {
                 if (col_max < grid[j][i]) {
                     col_max = grid[j][i];
                 }
             }
             col_maxes.push_back(col_max);
         }
-
-        int res = 0;
-        for (int r = 0; r != m; r++) {
+        
+        for (int r = 0; r != n; r++) {
+            int row_max = *max_element(grid[r].begin(), grid[r].end());
             for (int l = 0; l != n; l++) {
-                res += min(row_maxes[r], col_maxes[l]) - grid[r][l];
+                res += min(row_max, col_maxes[l]) - grid[r][l];
             }
         }
         return res;
