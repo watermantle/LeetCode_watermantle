f48599a17cc0e24e7ad31eb20716c0b05cccd228
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2021-10-24 23:49:41 -0400

    Time: 220 ms (34.03%), Space: 31.8 MB (11.62%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,5 +1,5 @@
+import numpy as np
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        C = Counter(nums)
-        for key, val in C.items():
-            if val > len(nums) / 2: return key
+        return int(np.median(np.array(nums)))
