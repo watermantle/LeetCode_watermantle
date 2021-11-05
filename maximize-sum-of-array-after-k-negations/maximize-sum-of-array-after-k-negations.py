185f535e6a08a6d35d040db1c41ba2008fd92266
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2021-11-05 10:29:26 -0400

    Time: 48 ms (79.97%), Space: 14.2 MB (81.77%) - LeetHub

--- maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
+++ maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.py
@@ -1,10 +1,8 @@
-from queue import PriorityQueue
-
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        pq = PriorityQueue()
-        for num in nums: pq.put(num)
-        for i in range(k):
-            num = pq.get()
-            pq.put(-num)
-        return sum([pq.get() for  _ in range(len(nums))])
+        nums.sort()
+        for i in range(len(nums)):
+            if k > 0 and nums[i] < 0:
+                nums[i] = -nums[i]
+                k -= 1
+        return sum(nums) - k % 2 * 2 * min(nums)
