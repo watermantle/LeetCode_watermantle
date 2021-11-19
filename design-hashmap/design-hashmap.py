aae9bf6a8794b44d848ced88bb9434a6bec24397
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2021-11-18 20:06:45 -0500

    Time: 900 ms (17.84%), Space: 17.3 MB (75.75%) - LeetHub

---
+++ design-hashmap/design-hashmap.py
@@ -0,0 +1,33 @@
+from bisect import bisect_left
+
+class MyHashMap:
+
+    def __init__(self):
+        self.key = []
+        self.value = []
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.key:
+            idx = self.key.index(key)
+            self.value[idx] = value
+        else:
+            self.key.append(key)
+            self.value.append(value)
+
+    def get(self, key: int) -> int:
+        if key not in self.key: return -1
+        else:
+            idx = self.key.index(key)
+            return self.value[idx]
+
+    def remove(self, key: int) -> None:
+        if key in self.key:
+            idx = self.key.index(key)
+            self.key = self.key[:idx] + self.key[idx+1:]
+            self.value = self.value[:idx] + self.value[idx+1:]
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
