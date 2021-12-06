1cc2c256639eee244a67544476ff5164486d1ac0
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2021-12-05 19:06:36 -0500

    Time: 36 ms (52.29%), Space: 14 MB (98.77%) - LeetHub

--- integer-break/integer-break.py
+++ integer-break/integer-break.py
@@ -1,13 +1,10 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        if n == 1 or n == 2: return 1
-        if n == 3: return 2
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 1
         
-        @lru_cache(None)
-        def helper(n):
-            res = n
-            
-            for i in range(1, n // 2 + 1):
-                res = max(helper(i) * helper(n-i), res)
-            return res
-        return helper(n)
+        for i in range(3, n+1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], (i - j) * j, dp[i - j] * j)
+        return dp[-1]
