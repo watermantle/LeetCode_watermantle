2ed8b762c8b40be48529b1e44aaa6a56a5d4289a
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-07-30 17:00:22 -0400

    Time: 87 ms (20.92%), Space: 41.3 MB (5.04%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
         vector<vector<string>> res;
-        for (string& str: strs) {
-            string key = str;
-            sort(key.begin(), key.end());
-            mp[key].push_back(str);
-        }
+        unordered_map<string, vector<string>> mp;
         
+        for (string& str: strs) {
+            mp[convert(str)].push_back(str);
+        }
         for (auto& item: mp) {
             res.push_back(item.second);
         }
         return res;
+    }
+private:
+    string convert(string& str) {
+        vector<int> temp(26, 0);
+        for (char& c: str) temp[c - 'a']++;
+        
+        stringstream ss;
+        for (int& i: temp) ss << "," << i;
+        cout << ss.str() << endl;
+        return ss.str();
     }
 };
