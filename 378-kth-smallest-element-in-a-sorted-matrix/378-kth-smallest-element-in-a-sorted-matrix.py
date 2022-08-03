d4db9070b9035291d1a6dfd473f3de27589b0f77
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-02 22:12:39 -0400

    Time: 294 ms (56.21%), Space: 18.7 MB (38.85%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,15 +1,33 @@
-import heapq
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def countLessEqual(matrix, mid, smaller, larger):
+            count, n = 0, len(matrix)
+            row, col = n - 1, 0
+
+            while row >= 0 and col < n:
+                if matrix[row][col] > mid:
+                    # if the number is greater than mid, keep the track of
+                    # the smallest number greater than mid
+                    larger = min(larger, matrix[row][col])
+                    row -= 1
+                else:
+                    smaller = max(smaller, matrix[row][col])
+                    col += 1
+                    count += row + 1
+                    
+            return count, smaller, larger
+            
         n = len(matrix)
-        heap_list = []
-        for i in range(min(k, n)):
-            heap_list.append((matrix[i][0], i, 0))
-        heapq.heapify(heap_list)
-        
-        while k:
-            ele, r, c = heapq.heappop(heap_list)
-            if c < n - 1:
-                heapq.heappush(heap_list, (matrix[r][c + 1], r, c + 1))
-            k -= 1
-        return ele
+        start, end = matrix[0][0], matrix[n - 1][n - 1]
+        while start < end:
+            mid = (end - start) / 2 + start
+            smaller, larger = matrix[0][0], matrix[n - 1][n - 1]
+            
+            count, smaller, larger = countLessEqual(matrix, mid, smaller, larger)
+            
+            if count == k: return smaller
+            if count < k: 
+                start = larger
+            else:
+                end = smaller
+        return start
