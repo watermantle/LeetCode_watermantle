f3d972f939fac5e138873b9e59eb3cd74f5fcc12
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-09 12:34:23 -0400

    Time: 288 ms (35.85%), Space: 67.5 MB (6.97%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -5,12 +5,14 @@ public:
         double dis_min = DBL_MAX;
         for (int i = 0; i < points.size(); ++i) {
             vector<int> point = points[i];
-            double dis = DBL_MAX;
-            if (point[0] == x) dis = abs(point[1] - y);
-            else if (point[1] == y) dis = abs(point[0] - x);
-            if (dis_min > dis) {
-                res = i;
-                dis_min = dis;
+            if (point[0] == x || point[1] == y) {
+                double dis;
+                if (point[0] == x) dis = abs(point[1] - y);
+                else if (point[1] == y) dis = abs(point[0] - x);
+                if (dis_min > dis) {
+                    res = i;
+                    dis_min = dis;
+                }
             }
         }
         return dis_min == DBL_MAX ? -1 : res;
