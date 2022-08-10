97607a52964fab7c8aee3c5e48c28b2f7f3624f1
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-09 22:40:39 -0400

    Time: 999 ms (19.80%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,29 +1,17 @@
 SELECT  product_id,
-        CASE
-            WHEN store1 IS NOT NULL THEN 'store1'
-        END AS store,
-        CASE
-            WHEN store1 IS NOT NULL THEN store1
-        END AS price
+        'store1' AS store,
+        store1 AS price
 FROM    Products
 WHERE   store1 IS NOT NULL
 UNION
 SELECT  product_id,
-        CASE
-            WHEN store2 IS NOT NULL THEN 'store2'
-        END AS store,
-        CASE
-            WHEN store2 IS NOT NULL THEN store2
-        END AS price
+        'store2' AS store,
+        store2 AS price
 FROM    Products
 WHERE   store2 IS NOT NULL
 UNION
 SELECT  product_id,
-        CASE
-            WHEN store3 IS NOT NULL THEN 'store3'
-        END AS store,
-        CASE
-            WHEN store3 IS NOT NULL THEN store3
-        END AS price
+        'store3' AS store,
+        store3 AS price
 FROM    Products
 WHERE store3 IS NOT NULL
