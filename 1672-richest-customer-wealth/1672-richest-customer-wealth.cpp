5bcbeaedf74620a710a4e4cdac52aa06e0e0b101
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-11 22:32:22 -0400

    Time: 6 ms (74.63%), Space: 7.8 MB (50.99%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -4,9 +4,8 @@ public:
         int res = 0;
         for (int m = 0; m < accounts.size(); ++m) {
             int sum = 0;
-            vector<int> row = accounts[m];
             for (int n = 0; n < accounts[0].size(); ++n) {
-                sum += row[n];
+                sum += accounts[m][n];
             }
             res = max(sum, res);
         }
