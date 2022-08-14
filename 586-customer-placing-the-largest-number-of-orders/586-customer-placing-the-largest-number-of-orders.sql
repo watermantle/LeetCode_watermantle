a4a9882c0a3679e28f9b7bdaddfecdd15f2d714d
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-14 11:38:28 -0400

    Time: 674 ms (39.62%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,12 @@
-SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(customer_number) DESC
-LIMIT 1
+SELECT  A.customer_number AS customer_number
+FROM    (
+    SELECT  customer_number,
+            ROW_NUMBER() OVER(PARTITION BY customer_number) AS seq
+    FROM Orders
+    ORDER BY seq DESC
+    LIMIT 1
+) AS A
+
+
+
 
