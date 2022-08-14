ce6eff32ed9a2f452f544144c5d06293ce75cedc
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-14 12:38:44 -0400

    Time: 46 ms (54.94%), Space: 13.8 MB (55.49%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,6 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        command = command.replace("()", "o")
+        command = command.replace("(", "")
+        command = command.replace(")", "")
+        return command
