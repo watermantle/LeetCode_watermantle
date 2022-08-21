df354fbd448b5ed0033f048557d17694ebe341f3
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-21 13:01:24 -0400

    Time: 1124 ms (66.15%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -1,20 +1,24 @@
-SELECT  RF.name AS country
+SELECT  name AS country
 FROM    (
-    SELECT  P.id,
-            duration
+    SELECT  TC.id,
+            RF.name,
+            AVG(duration) AS avg_dur
+    FROM    (
+        SELECT  P.id,
+                duration
+        FROM    Person AS P
+        LEFT JOIN Calls AS CA
+        ON      P.id = CA.caller_id OR P.id = CA.callee_id
+    ) AS TC
+    LEFT JOIN
+    (SELECT  id,
+            C.name,
+            country_code
     FROM    Person AS P
-    LEFT JOIN Calls AS CA
-    ON      P.id = CA.caller_id OR P.id = CA.callee_id
-) AS TC
-LEFT JOIN
-(SELECT  id,
-        C.name,
-        country_code
-FROM    Person AS P
-LEFT JOIN Country AS C
-ON      C.country_code = SUBSTRING(P.phone_number, 1, 3)
-) AS RF
-ON TC.id = RF.id
-GROUP BY RF.country_code
-HAVING AVG(TC.duration) > (SELECT AVG(duration) FROM Calls)
-
+    LEFT JOIN Country AS C
+    ON      C.country_code = SUBSTRING(P.phone_number, 1, 3)
+    ) AS RF
+    ON TC.id = RF.id
+    GROUP BY RF.country_code
+) AS a
+WHERE  avg_dur > (SELECT AVG(duration) FROM Calls)
