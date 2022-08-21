21ead917388b6fe118e1cc0595f0516254da9034
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-21 12:52:28 -0400

    Time: 1616 ms (39.94%), Space: 0B (100.00%) - LeetHub

---
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -0,0 +1,30 @@
+SELECT  name AS country
+FROM    (
+    SELECT  TC.id,
+            RF.name,
+            AVG(duration) AS avg_dur
+    FROM    (
+        SELECT  P.id,
+                duration
+        FROM    Person AS P
+        LEFT JOIN Calls AS CA
+        ON      P.id = CA.caller_id
+        UNION
+        SELECT  P.id,
+                duration
+        FROM    Person AS P
+        LEFT JOIN Calls AS CA
+        ON      P.id = CA.callee_id
+    ) AS TC
+    LEFT JOIN
+    (SELECT  id,
+            C.name,
+            country_code
+    FROM    Person AS P
+    LEFT JOIN Country AS C
+    ON      C.country_code = SUBSTRING(P.phone_number, 1, 3)
+    ) AS RF
+    ON TC.id = RF.id
+    GROUP BY RF.country_code
+) AS a
+WHERE  avg_dur > (SELECT AVG(duration) FROM Calls)
