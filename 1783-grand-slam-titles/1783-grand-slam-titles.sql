01f8b73033775347dcc75207015bf77c46058ab9
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-21 23:30:45 -0400

    Time: 1687 ms (48.33%), Space: 0B (100.00%) - LeetHub

--- 1783-grand-slam-titles/1783-grand-slam-titles.sql
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -1,31 +1,7 @@
-SELECT  id AS player_id,
+SELECT  player_id,
         player_name,
-        grand_slams_count
-FROM
-(SELECT  id,
-        SUM(num) AS grand_slams_count
-FROM    (
-    SELECT  Wimbledon AS id,
-            COUNT(Wimbledon) AS num
-    FROM    Championships
-    GROUP BY Wimbledon
-    UNION ALL
-    SELECT  Fr_open AS id,
-            COUNT(Fr_open) AS num
-    FROM    Championships
-    GROUP BY Fr_open
-    UNION ALL
-    SELECT  US_open AS id,
-            COUNT(US_open) AS num
-    FROM    Championships
-    GROUP BY US_open
-    UNION ALL
-    SELECT  Au_open AS id,
-            COUNT(Au_open) AS num
-    FROM    Championships
-    GROUP BY Au_open
-) AS C
-GROUP BY id
-) AS B
-LEFT JOIN Players AS P
-ON B.id = P.player_id
+        SUM(Wimbledon = player_id) + SUM(Fr_open = player_id) + SUM(US_open = player_id) + SUM(Au_open = player_id) AS grand_slams_count
+FROM    Championships AS C
+JOIN    Players AS P
+ON      P.player_id = C.Wimbledon OR P.player_id = C.Fr_open OR P.player_id = C.US_open OR P.player_id = C.Au_open
+GROUP BY player_id
