6e67a7967f978b890d93498c53399efb7307f998
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-24 23:52:18 -0400

    Time: 2744 ms (39.61%), Space: 0B (100.00%) - LeetHub

--- 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
+++ 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
@@ -3,7 +3,7 @@ SELECT  product_name,
         A.order_id,
         A.order_date
 FROM    Products AS P
-LEFT JOIN (
+JOIN (
     SELECT  product_id,
             order_id,
             order_date,
@@ -11,7 +11,7 @@ LEFT JOIN (
     FROM    Orders AS O
 ) AS A
 ON      P.product_id = A.product_id
-WHERE   A.order_date IS NOT NULL
-AND     A.seq = 1
+# WHERE   A.order_date IS NOT NULL
+WHERE     A.seq = 1
 ORDER BY product_name ASC, A.product_id ASC, A.order_id ASC
 
