67cd510fd7b46ea4bafe89af2c44a7674dee6b4a
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-25 22:28:00 -0400

    Time: 1132 ms (61.53%), Space: 20.4 MB (39.01%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,12 +1,15 @@
+from queue import PriorityQueue
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        arr = []
+        pq = PriorityQueue()
         for point in points:
             dis_sq = point[0] ** 2 + point[1] ** 2
-            arr.append((point, dis_sq))
-        arr.sort(key=lambda x: x[-1])
+            pq.put((dis_sq, point))
         
         res = []
-        for i in range(k):
-            res.append(arr[i][0])
+        for _ in range(k):
+            item = pq.get()
+            res.append(item[1])
         return res
+        
+        
