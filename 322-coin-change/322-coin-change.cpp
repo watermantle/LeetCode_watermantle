49bc1f008bfce573947c46617277ad37daf971c8
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-25 23:45:02 -0400

    Time: 118 ms (68.71%), Space: 14.6 MB (54.56%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 10001);
+        dp[0] = 0;
+        for (int& coin: coins) {
+            for (int j = coin; j <= amount; ++j) {
+                dp[j] = min(dp[j], dp[j - coin] + 1);
+            }
+        }
+        return dp[amount] < 10001 ? dp[amount] : -1;
+    }
+};
