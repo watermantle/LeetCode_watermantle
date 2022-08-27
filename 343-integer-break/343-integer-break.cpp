ef81cfda92a162cd579774f7a8e3027c184b1afb
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-08-27 09:49:44 -0400

    Time: 5 ms (21.61%), Space: 6.2 MB (50.33%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int integerBreak(int n) {
         vector<int> dp(n + 1, 0);
-        dp[1] = 1; dp[2] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
         
-        for (int i = 3; i != n+1; i++){
-            for (int j = 1; j != i; j++){
-                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+        for (int i = 3; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                // we need to consider dp[i - j] * dp[j] as well, but obvisouly, it always gives the min outcome
+                // for example, if i = 4, j = 2, dp[i-j] = dp[2] = 1, dp[i - j] * j = 1 * 2 = 2
+                // (i-j) * j= (4-2) * 2 = 4
+                dp[i] = max(dp[i], max(dp[i - j] * j, (i - j) * j));
             }
         }
         return dp[n];
