945331b7f0e60bd6ee1b7c6830262382ff8a59ae
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-09-10 11:23:46 -0400

    Time: 7 ms (92.20%), Space: 10.9 MB (84.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -13,37 +13,14 @@ public:
             return res;
         }
         
-        int dp[1000][101][2] = {};
+        vector<int> buy(k + 1, INT_MIN), sell(k + 1, 0);
         
-        // init with inf and -inf
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                dp[i][j][0] = -1000000000;
-                dp[i][j][1] = -1000000000;
+        for (int& price: prices) {
+            for (int i = 1; i <= k; ++i) {
+                buy[i] = max(buy[i], sell[i - 1] - price);
+                sell[i] = max(sell[i], buy[i] + price);
             }
         }
-        
-        dp[0][0][0] = 0;
-        dp[0][1][1] = -prices[0];
-        
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                // 0 meaning no stock, eiter keep not holding
-                // or selling the stock
-                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
-                if (j > 0) {
-                    // 1 meaning it holds stocks
-                    // either holding or buying stock 
-                    dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
-                }
-            }
-        }
-        
-        int res = 0;
-        for (int j = 0; j <= k; ++j) {
-            res = max(res, dp[n - 1][j][0]);
-        }
-        return res;                            
+        return sell[k];
     }
 };
