8633470c9e42f6e5cdc934fd092b983e2d178aae
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-09-14 18:37:14 -0400

    Time: 3850 ms (22.25%), Space: 14.4 MB (38.22%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.py
@@ -9,5 +9,4 @@ class Solution:
             for j in range(i):
                 if pairs[i][0] > pairs[j][1]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
-        
+        return dp[-1]
