cb6a6817df0a0380d1d9c6151d9cc2bf078d8ee6
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-09-15 22:38:26 -0400

    Time: 439 ms (27.17%), Space: 11.8 MB (19.04%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -7,9 +7,9 @@ public:
         vector<ull> dp(n + 1, INT_MAX);
         dp[0] = 0;
         
-        for (int i = 1; i <= n; ++i) {
-            for (ull& sq_num: sq_nums) {
-                if (i >= sq_num) dp[i] = min(dp[i], dp[i - sq_num] + 1);
+        for (ull& num: sq_nums) {
+            for (int i = num; i <= n; ++i) {
+                dp[i] = min(dp[i], dp[i - num] + 1);
             }
         }
         return dp[n];
