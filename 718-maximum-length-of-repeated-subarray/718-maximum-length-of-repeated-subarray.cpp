28bb28e131c70adcf0774745e3cd0e5052a20810
Bai, Yu <43456665+watermantle@users.noreply.github.com>
2022-09-20 09:08:59 -0400

    Time: 644 ms (29.84%), Space: 109.2 MB (14.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -4,10 +4,10 @@ public:
         int n1 = nums1.size(), n2 = nums2.size();
         vector<vector<int>> dp(n1 + 1, vector<int> (n2 + 1, 0));
         int res = 0;
-        for (int i = n1 - 1; i >= 0; --i) {
-            for (int j = n2 - 1; j >= 0; --j) {
-                if (nums1[i] == nums2[j]) {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                     res = max(res, dp[i][j]);
                 }
             }
